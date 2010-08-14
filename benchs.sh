29592d7025a9a7daee3548dfd42842fbfcab0350
Rolando Espinoza La fuente <darkrho@gmail.com>
2010-08-14 00:00:22 -0400

    added tornado urls to benchs.sh Sign-Off: Rolando Espinoza La fuente

--- benchs.sh
+++ benchs.sh
@@ -1,4 +1,29 @@
-URLS="http://localhost/haanga_benchs/b1.php http://localhost/h2o_benchs/b1.php http://localhost:8000/b1/ http://localhost/haanga_benchs/b1_tpl.php http://localhost/h2o_benchs/b1_tpl.php http://localhost:8000/b1_tpl/ http://localhost/haanga_benchs/b2_tpl.php http://localhost/h2o_benchs/b2_tpl.php http://localhost:8000/b2_tpl/ http://localhost/haanga_benchs/b2_1_tpl.php http://localhost/h2o_benchs/b2_1_tpl.php http://localhost:8000/b2_1_tpl/ http://localhost/haanga_benchs/header.php http://localhost/h2o_benchs/header.php http://localhost:8000/header/"
+URLS="
+http://localhost/haanga_benchs/b1.php
+http://localhost/h2o_benchs/b1.php
+http://localhost:8000/b1/
+http://localhost:8888/b1/
+
+http://localhost/haanga_benchs/b1_tpl.php
+http://localhost/h2o_benchs/b1_tpl.php
+http://localhost:8000/b1_tpl/
+http://localhost:8888/b1_tpl/
+
+http://localhost/haanga_benchs/b2_tpl.php
+http://localhost/h2o_benchs/b2_tpl.php
+http://localhost:8000/b2_tpl/
+http://localhost:8888/b2_tpl/
+
+http://localhost/haanga_benchs/b2_1_tpl.php
+http://localhost/h2o_benchs/b2_1_tpl.php
+http://localhost:8000/b2_1_tpl/
+http://localhost:8888/b2_1_tpl/
+
+http://localhost/haanga_benchs/header.php
+http://localhost/h2o_benchs/header.php
+http://localhost:8000/header/
+http://localhost:8888/header/
+"
 
 for url in $URLS
 do
